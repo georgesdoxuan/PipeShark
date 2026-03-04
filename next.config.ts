48782912e075b14d3d93288f15ddd357ecdc0633
georgesdoxuan <gobroofficiel@gmail.com>
2026-03-04 15:46:13 +0100

    “efjubhd”

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
       { source: "/", destination: "/landing", permanent: false },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/_next/static/chunks/:name.js",
+        headers: [{ key: "Content-Type", value: "application/javascript" }],
+      },
+      {
+        source: "/_next/static/chunks/:name.css",
+        headers: [{ key: "Content-Type", value: "text/css" }],
+      },
+      {
+        source: "/_next/static/media/:path*",
+        headers: [{ key: "Content-Type", value: "font/woff2" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
